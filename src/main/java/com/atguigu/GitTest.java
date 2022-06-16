159521ac26aed69018baad506ae39f36e3e8c7df
Heika <heika2022@163.com>
2022-06-16 13:54:02 +0800

    mst2 commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello Git3");
         System.out.println("Master Test");
         System.out.println("Hot fix Test");
+        System.out.println("我原本以为吕布已经天下无敌");
     }
 }
